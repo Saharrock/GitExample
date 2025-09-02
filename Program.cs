cdf840f4f5e9dcb1aefb39b5c3d82647fea98ec8
Saharrock <salnikovsergej76@gmail.com>
2025-09-02 13:29:06 +0300

    i added Maxims family name

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace GitExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Maxim");
+            Console.WriteLine("Hello Maxim Shipanovski");
         }
     }
 }
